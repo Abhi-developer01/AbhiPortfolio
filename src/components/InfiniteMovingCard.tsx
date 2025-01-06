e18e9abce3ab6cfb8acea324dba0d335eea0da9d
Abhi-developer01 <theabhishek96@gmail.com>
2025-01-06 17:59:43 +0530

    increades infinite moving cards visible area

--- src/components/InfiniteMovingCard.tsx
+++ src/components/InfiniteMovingCard.tsx
@@ -39,7 +39,7 @@ function MusicSchoolTestimonials() {
     <div className="h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
         <h2 className="text-3xl font-bold text-center mb-8 z-10">Hear our Harmony: Voices of success</h2>
         <div className="flex justify-center w-full  h-auto lg:h-auto px-4 sm:px-6 ">
-            <div className="w-full flex justify-center pt-100 max-w-6xl">
+            <div className="w-full flex justify-center pt-100 max-w-full">
             <InfiniteMovingCards
                 items={musicSchoolTestimonials}
                 direction="right"
